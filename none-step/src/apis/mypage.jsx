c94bd52211ce184cc1a7bcff5a44ba87f8108730
miseullang <miseullang@gmail.com>
2024-07-26 15:55:31 +0900

    :sparkles:feat : authAxios 추가

---
+++ none-step/src/apis/mypage.jsx
@@ -0,0 +1,8 @@
+import { getAuthAxios } from "./authAxios";
+
+export const getMyPage = async () => {
+  const access = localStorage.getItem('access');
+  const authAxios = getAuthAxios(access);
+  const result = await authAxios.get('/mypage');
+  return result.data;
+};
--- none-step/src/routes/AppRoutes.jsx
+++ none-step/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Router, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import TermsOfService from "../pages/TermsOfService";
 import SignUpForm from "../pages/SignUpForm";
+import MyPage from "../pages/MyPage";
 
 function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ function AppRoutes() {
             <Route index element={<TermsOfService />}/>
             <Route path="/signUp" element={<SignUpForm />}/>
             <Route path="/login" element={<Login />} />
+            <Route path="/mypage" element={<MyPage />} />
         </Routes>
     );
 }
