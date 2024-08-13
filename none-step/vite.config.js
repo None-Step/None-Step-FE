af728f14734330abacaaade23b92f52922e69c71
WHOOZ-23 <riahboa@naver.com>
2024-08-13 16:41:22 +0900

    :sparkles:feat: 지도 페이지 구현

--- none-step/vite.config.js
+++ none-step/vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@", replacement: resolve(__dirname, "src") },
+            { find: "@apis", replacement: resolve(__dirname, "src/apis") },
+            {
+                find: "@assets",
+                replacement: resolve(__dirname, "src/assets"),
+            },
+            {
+                find: "@components",
+                replacement: resolve(__dirname, "src/components"),
+            },
+            {
+                find: "@hooks",
+                replacement: resolve(__dirname, "src/hooks"),
+            },
+            {
+                find: "@layout",
+                replacement: resolve(__dirname, "src/layout"),
+            },
+            { find: "@pages", replacement: resolve(__dirname, "src/pages") },
+            {
+                find: "@routes",
+                replacement: resolve(__dirname, "src/routes"),
+            },
+            { find: "@store", replacement: resolve(__dirname, "src/store") },
+            {
+                find: "@styles",
+                replacement: resolve(__dirname, "src/styles"),
+            },
+        ],
+    },
+});
