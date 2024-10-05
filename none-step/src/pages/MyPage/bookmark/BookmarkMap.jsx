5d3ae6e448be7b0523bd8dc921de7b00580101a4
miseullang <miseullang@gmail.com>
2024-10-05 14:47:05 +0900

    :sparkles:feat: 지도 북마크 경로 추가

---
+++ none-step/src/pages/MyPage/bookmark/BookmarkMap.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { BG, PageContainer } from '../MyPage.style';
+import MenuBar from "@/components/menuBar/MenuBar";
+import { PageHeader } from "@/components/header/Headers";
+import { BookmarkPlace, BookmarkPath } from './BookmarkList';
+
+const Wrapper = styled.div`
+    width: 100%;
+    padding-top: 70px;
+`;
+
+const TabContainer = styled.div`
+  display: flex;
+  background-color: ${(props) => props.theme.colors.white};
+`;
+
+const Tab = styled.button`
+  font-size: 1.4rem;
+  flex: 1;
+  padding: 1.5rem;
+  background: none;
+  border: none;
+  border-bottom: 2px solid ${(props) => props.$active ? props.theme.colors.primary : props.theme.colors.gray06};
+  color: ${(props) => props.$active ? props.theme.colors.primary : props.theme.colors.gray02};
+  font-weight: ${(props) => props.$active ? 'bold' : 'normal'};
+  cursor: pointer;
+`;
+
+const TabContent = styled.div`
+  display: ${(props) => props.$active ? 'block' : 'none'};
+`;
+
+const BookmarkMap = ({color}) => {
+  const [activeTab, setActiveTab] = useState('장소');
+
+  return (
+    <BG>
+      <PageContainer>
+          <PageHeader />
+
+          <Wrapper>
+
+            <TabContent $active={activeTab === '장소'}>
+              <BookmarkPlace
+                color={color}
+                placeName='집'
+                placeAddress='부산광역시 서구 송도해변로' />
+            </TabContent>
+
+            <TabContent $active={activeTab === '경로'}>
+                <BookmarkPath
+                  color={color}
+                  originName='집'
+                  destinationName='회사'
+                  />
+            </TabContent>
+
+          </Wrapper>
+
+          <MenuBar />
+      </PageContainer>
+    </BG>
+
+  )
+}
+
+export default BookmarkMap
