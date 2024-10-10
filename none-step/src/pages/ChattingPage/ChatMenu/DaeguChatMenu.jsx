ab0f7d348c4cba5dee8c68216b97f8f8d2912229
WHOOZ-23 <riahboa@naver.com>
2024-10-10 10:42:19 +0900

    :sparkles:feat: 채팅 목록 최근 채팅 시간 표시

--- none-step/src/pages/ChattingPage/ChatMenu/DaeguChatMenu.jsx
+++ none-step/src/pages/ChattingPage/ChatMenu/DaeguChatMenu.jsx
@@ -2,10 +2,58 @@ import daeguMetroIcon from "@assets/icons/daegu-transp-corp-logo.svg";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectedCategory } from "@store/slices/categorySlice";
+import { useEffect, useState } from "react";
+import axiosInstance from "@apis/axiosInstance";
 
 const DaeguChatMenu = () => {
+    const [chatTimes, setChatTimes] = useState([]);
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    useEffect(() => {
+        axiosInstance
+            .get(`/nonestep/chat/list?region=daegu`)
+            .then((response) => {
+                setChatTimes(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const chattingDate = (lineValue) => {
+        const chatDate = chatTimes.find((data) => data.line === lineValue);
+
+        if (!chatDate) return;
+
+        const pastDate = new Date(chatDate.date);
+        pastDate.setHours(pastDate.getHours() + 9);
+
+        const now = Date.now();
+        const milliseconds = now - pastDate.getTime();
+        const minutes = Math.floor(milliseconds / 1000 / 60);
+
+        if (minutes < 1) {
+            return "방금 전";
+        } else if (minutes < 60) {
+            return `${minutes}분 전`;
+        } else if (minutes < 1440) {
+            const hours = Math.floor(minutes / 60);
+
+            return `${hours}시간 전`;
+        } else {
+            const days = Math.floor(minutes / 1440);
+
+            if (days < 30) {
+                return `${days}일 전`;
+            } else {
+                const month = Math.floor(days / 30);
+
+                return `${month}달 전`;
+            }
+        }
+    };
 
     const handleClickMenu = (line, region) => {
         navigate("/chat/daegu");
@@ -26,6 +74,7 @@ const DaeguChatMenu = () => {
             >
                 <span className="daegu_line1">1</span>
                 <span>1호선</span>
+                <span className="chatting_time">{chattingDate("line1")}</span>
             </li>
             <li
                 className="line2"
@@ -33,6 +82,7 @@ const DaeguChatMenu = () => {
             >
                 <span className="daegu_line2">2</span>
                 <span>2호선</span>
+                <span className="chatting_time">{chattingDate("line2")}</span>
             </li>
             <li
                 className="line3"
@@ -40,6 +90,7 @@ const DaeguChatMenu = () => {
             >
                 <span className="daegu_line3">3</span>
                 <span>3호선</span>
+                <span className="chatting_time">{chattingDate("line3")}</span>
             </li>
             <img src={daeguMetroIcon} alt="daegu-metro-icon" />
         </>
--- none-step/src/pages/ChattingPage/ChatMenu/GwangjuChatMenu.jsx
+++ none-step/src/pages/ChattingPage/ChatMenu/GwangjuChatMenu.jsx
@@ -2,10 +2,58 @@ import gwangjuMetroIcon from "@assets/icons/gwangju-transp-corp-logo.svg";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectedCategory } from "@store/slices/categorySlice";
+import { useEffect, useState } from "react";
+import axiosInstance from "@apis/axiosInstance";
 
 const GwangjuChatMenu = () => {
+    const [chatTimes, setChatTimes] = useState([]);
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    useEffect(() => {
+        axiosInstance
+            .get(`/nonestep/chat/list?region=gwangju`)
+            .then((response) => {
+                setChatTimes(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const chattingDate = (lineValue) => {
+        const chatDate = chatTimes.find((data) => data.line === lineValue);
+
+        if (!chatDate) return;
+
+        const pastDate = new Date(chatDate.date);
+        pastDate.setHours(pastDate.getHours() + 9);
+
+        const now = Date.now();
+        const milliseconds = now - pastDate.getTime();
+        const minutes = Math.floor(milliseconds / 1000 / 60);
+
+        if (minutes < 1) {
+            return "방금 전";
+        } else if (minutes < 60) {
+            return `${minutes}분 전`;
+        } else if (minutes < 1440) {
+            const hours = Math.floor(minutes / 60);
+
+            return `${hours}시간 전`;
+        } else {
+            const days = Math.floor(minutes / 1440);
+
+            if (days < 30) {
+                return `${days}일 전`;
+            } else {
+                const month = Math.floor(days / 30);
+
+                return `${month}달 전`;
+            }
+        }
+    };
 
     const handleClickMenu = (line, region) => {
         navigate("/chat/gwangju");
@@ -26,6 +74,7 @@ const GwangjuChatMenu = () => {
             >
                 <span className="gwangju_line1">1</span>
                 <span>1호선</span>
+                <span className="chatting_time">{chattingDate("line1")}</span>
             </li>
             <img src={gwangjuMetroIcon} alt="gwangju-metro-icon" />
         </>
