11a141ca18517262138eee0cae8545a7bd3566d7
WHOOZ-23 <riahboa@naver.com>
2024-10-13 03:29:08 +0900

    :art:add: header 공지사항 추가

--- none-step/src/components/header/Headers.jsx
+++ none-step/src/components/header/Headers.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useLocation, useNavigate } from 'react-router-dom'
-import logoImage from '@/assets/img/header-logo.svg'
-import LogInIcon from '@/assets/img/user.svg'
-import LogOutIcon from '@/assets/img/logout.svg'
-import LeftArrow from '@/assets/img/LeftArrow.svg'
-import { HeaderWrapper, Icon, IconAuthState, MainWrapper, PageName } from './Headers.style'
-import useLogout from '@/hooks/logout'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import logoImage from "@/assets/img/header-logo.svg";
+import LogInIcon from "@/assets/img/user.svg";
+import LogOutIcon from "@/assets/img/logout.svg";
+import LeftArrow from "@/assets/img/LeftArrow.svg";
+import {
+    HeaderWrapper,
+    Icon,
+    IconAuthState,
+    MainWrapper,
+    PageName,
+} from "./Headers.style";
+import useLogout from "@/hooks/logout";
 
 export const MainHeader = () => {
     const isAuthorized = useSelector((state) => state.member.isAuthorized);
@@ -21,18 +27,17 @@ export const MainHeader = () => {
         }
     };
 
-  return (
-    <MainWrapper>
-      <img src={logoImage} width="130px" alt="로고" />
-      <IconAuthState
-        src={isAuthorized ? LogOutIcon : LogInIcon}
-        alt={isAuthorized ? "로그아웃 아이콘" : "로그인 아이콘"}
-        onClick={handleIconClick}
-      />
-    </MainWrapper>
-  )
-}
-
+    return (
+        <MainWrapper>
+            <img src={logoImage} width="130px" alt="로고" />
+            <IconAuthState
+                src={isAuthorized ? LogOutIcon : LogInIcon}
+                alt={isAuthorized ? "로그아웃 아이콘" : "로그인 아이콘"}
+                onClick={handleIconClick}
+            />
+        </MainWrapper>
+    );
+};
 
 export const PageHeader = () => {
     const isAuthorized = useSelector((state) => state.member.isAuthorized);
@@ -40,7 +45,6 @@ export const PageHeader = () => {
     const navigate = useNavigate();
     const currentPath = location.pathname;
     const handleLogout = useLogout();
-
 
     const handleNavigation = () => {
         navigate(-1);
@@ -55,6 +59,14 @@ export const PageHeader = () => {
     };
 
     const getPageName = () => {
+        if (/^\/notice\/\d+$/.test(currentPath)) {
+            return "공지사항";
+        }
+
+        if (/^\/notice\/modify\/\d+$/.test(currentPath)) {
+            return "공지사항 수정";
+        }
+
         switch (currentPath) {
             case "/findway":
                 return "길찾기";
@@ -78,6 +90,12 @@ export const PageHeader = () => {
                 return "대구";
             case "/chat/gwangju":
                 return "광주";
+            case "/notice":
+                return "공지사항";
+            case "/notice/edit":
+                return "공지사항 작성";
+            case "/notice/search":
+                return "공지사항 검색";
             default:
                 return "";
         }
