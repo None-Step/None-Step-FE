2ec4c2fecbb384fa5e9678ae8742d14572bc8fdf
WHOOZ-23 <riahboa@naver.com>
2024-11-06 00:20:19 +0900

    :lipstick:style: 회원가입/아이디 찾기/비밀번호 찾기 시 재발송 버튼 글씨 밀림 현상 수정

--- none-step/src/pages/SignUp/SignUp02/SignUpForm.style.jsx
+++ none-step/src/pages/SignUp/SignUp02/SignUpForm.style.jsx
@@ -2,35 +2,36 @@ import styled from "styled-components";
 import InputForm from "@/components/InputForm";
 
 export const PageTitle = styled.span`
-  font-size: 1.4rem;
-  color: ${(props) => props.theme.colors.gray01};
-  margin-bottom: 3rem;
+    font-size: 1.4rem;
+    color: ${(props) => props.theme.colors.gray01};
+    margin-bottom: 3rem;
 `;
 
 export const InputWrap = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 export const SubmitBut = styled.button`
-  width: 90px;
-  height: 50px;
-  padding: 1.7rem 2rem;
-  box-sizing: border-box;
-  border: none;
-  border-radius: 6px;
-  cursor: pointer;
-  background-color: ${(props) => props.theme.colors.primary};
-  color: ${(props) => props.theme.colors.white};
-  margin-left: 0.5rem;
+    width: 90px;
+    height: 50px;
+    padding: 0 1.5rem;
+    box-sizing: border-box;
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+    background-color: ${(props) => props.theme.colors.primary};
+    color: ${(props) => props.theme.colors.white};
+    font-size: 1.4rem;
+    margin-left: 0.5rem;
 
-  &:disabled {
-    background-color: ${(props) => props.theme.colors.gray02};
-  }
+    &:disabled {
+        background-color: ${(props) => props.theme.colors.gray02};
+    }
 `;
 
 export const MarginInputForm = styled(InputForm)`
-  margin-bottom: 1.5rem;
+    margin-bottom: 1.5rem;
 `;
